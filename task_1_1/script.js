39f8eea12a676d022efb5bf51c1cf24dd7025576
AndreyYablokov <78611870+AndreyYablokov@users.noreply.github.com>
2021-09-02 11:34:33 +0300

    Update script.js

--- task_1_1/script.js
+++ task_1_1/script.js
@@ -1,3 +1,3 @@
 var tempInCelsius = prompt('Введите температуру в градусах Цельсия: ');
 var tempInFahrenheit = (9 / 5) * tempInCelsius + 32;
-alert(`${tempInCelsius} \u00B0C = ${tempInFahrenheit} \u00B0F`)
+alert(`${tempInCelsius} \u00B0C = ${tempInFahrenheit} \u00B0F`);
